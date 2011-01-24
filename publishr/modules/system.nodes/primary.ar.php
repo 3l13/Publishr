a46da8936ffeef57eefcf697b5ea5f085d05e32b
Olivier Laviale <contact@weirdog.com>
2011-01-24 02:09:36 +0100

    Added the _css_class_ getter

--- publishr/modules/system.nodes/primary.ar.php
+++ publishr/modules/system.nodes/primary.ar.php
@@ -1,11 +1,11 @@
 <?php
 
 /**
- * This file is part of the WdPublisher software
+ * This file is part of the Publishr software
  *
  * @author Olivier Laviale <olivier.laviale@gmail.com>
  * @link http://www.wdpublisher.com/
- * @copyright Copyright (c) 2007-2010 Olivier Laviale
+ * @copyright Copyright (c) 2007-2011 Olivier Laviale
  * @license http://www.wdpublisher.com/license.html
  */
 
@@ -202,7 +202,7 @@ class system_nodes_WdActiveRecord extends WdActiveRecord
 	 * Return the native node for this translated node.
 	 */
 
-	// TODO-20100629: Maybe we should rename the `tnid` property to `native_nid`
+	// TODO-20100629: Maybe we should rename the `tnid` property to `nativeid`
 
 	protected function __get_native()
 	{
@@ -212,5 +212,10 @@ class system_nodes_WdActiveRecord extends WdActiveRecord
 		}
 
 		return $this;
+	}
+
+	protected function __get_css_class()
+	{
+		return "node node-{$this->nid} node-slug-{$this->slug} constructor-" . wd_normalize($this->constructor);
 	}
 }
